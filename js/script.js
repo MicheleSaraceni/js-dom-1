7b75e0e8ffadeae4da1886f18e2e1bec1fd6536b
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-07 22:42:53 +0100

    ended exercise

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 "use strict";
 console.clear();
 
+const press = document.getElementById("button");
+console.log(press);
+const lampadina = document.querySelector("img");
+console.log(lampadina);
+
+press.addEventListener("click", () => {
+    console.log(lampadina.src);
+    if (lampadina.src.includes("/img/white_lamp.png")){
+        lampadina.src = "/img/yellow_lamp.png";
+        lampadina.alt = "LampadinaAccesa";
+        
+    } else if (lampadina.src.includes("/img/yellow_lamp.png")){
+        lampadina.src = "/img/white_lamp.png";
+        lampadina.alt = "LampadinaSpenta";
+    }
+})
