3d7114c33140b8cd2c0bca1b18ad8398400f7e96
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-07 23:07:31 +0100

    fixed lamp position and effect

--- js/script.js
+++ js/script.js
@@ -5,15 +5,22 @@ const press = document.getElementById("button");
 console.log(press);
 const lampadina = document.querySelector("img");
 console.log(lampadina);
+const titolo = document.querySelector("h1");
+const body = document.querySelector("body");
 
 press.addEventListener("click", () => {
     console.log(lampadina.src);
     if (lampadina.src.includes("/img/white_lamp.png")){
         lampadina.src = "/img/yellow_lamp.png";
         lampadina.alt = "LampadinaAccesa";
-        
+        lampadina.style.paddingRight = "82px";
+        titolo.style.color = "black";
+        body.style.backgroundColor = "white"
     } else if (lampadina.src.includes("/img/yellow_lamp.png")){
         lampadina.src = "/img/white_lamp.png";
         lampadina.alt = "LampadinaSpenta";
+        lampadina.style.paddingRight = "0";
+        titolo.style.color = "white";
+        body.style.backgroundColor = "black"
     }
 })
